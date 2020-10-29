9baca3214cbed1ff3a16ede201181d3b4e9d5c5d
mikkelpm <mikkel.p.m@gmail.com>
2020-10-28 21:33:58 -0400

    Coverage of identified set + max lag length in simulations

--- simulations/print_results.m
+++ simulations/print_results.m
@@ -30,14 +30,16 @@ FVR_hor = dgps{1}.settings.FVR_hor;
 n_hor = length(FVR_hor);
 
 dgp_no = nan(n_dgp,1);
-R2_inv_true = nan(n_dgp,1);
-R2_inv_cov = nan(n_dgp,1);
-FVR_true = nan(n_dgp,n_hor);
-FVR_cov = nan(n_dgp,n_hor);
-FVR_cov_svar = nan(n_dgp,n_hor);
+true_R2_inv = nan(n_dgp,1);
+cov_par_R2_inv = nan(n_dgp,1);
+cov_set_R2_inv = nan(n_dgp,1);
+true_FVR = nan(n_dgp,n_hor);
+cov_par_FVR = nan(n_dgp,n_hor);
+cov_set_FVR = nan(n_dgp,n_hor);
+cov_par_FVR_svar = nan(n_dgp,n_hor);
 
-cover1 = @(cis,param) nanmean(cis(1,:)<=param & param<=cis(2,:), 2);
-cover2 = @(cis,param) nanmean(reshape(cis(:,FVR_var,1,:)<=param & param<=cis(:,FVR_var,2,:), n_hor, []), 2);
+cover1 = @(cis,param_lb,param_ub) mean(cis(1,:)<=param_lb & param_ub<=cis(2,:), 2);
+cover2 = @(cis,param_lb,param_ub) mean(reshape(cis(:,FVR_var,1,:)<=param_lb(:,FVR_var) & param_ub(:,FVR_var)<=cis(:,FVR_var,2,:), n_hor, []), 2);
 
 for i_dgp=1:n_dgp % For each DGP...
     
@@ -45,25 +47,29 @@ for i_dgp=1:n_dgp % For each DGP...
     dgp_no(i_dgp) = dgps{i_dgp}.model.dgp;
     
     % Degree of invertibility
-    R2_inv_true(i_dgp) = dgps{i_dgp}.model.R2_inv;
-    R2_inv_cov(i_dgp) = cover1(dgps{i_dgp}.svma_R2_inv_cis, R2_inv_true(i_dgp));
+    true_R2_inv(i_dgp) = dgps{i_dgp}.model.R2_inv;
+    cov_par_R2_inv(i_dgp) = cover1(dgps{i_dgp}.svma_R2_inv_cis, dgps{i_dgp}.model.R2_inv, dgps{i_dgp}.model.R2_inv);
+    cov_set_R2_inv(i_dgp) = cover1(dgps{i_dgp}.svma_R2_inv_cis, dgps{i_dgp}.bounds_pop.R2_inv_LB, dgps{i_dgp}.bounds_pop.R2_inv_UB);
     
     % FVR
-    FVR_true(i_dgp,:) = dgps{i_dgp}.model.FVR(:,FVR_var);
-    FVR_cov(i_dgp,:) = cover2(dgps{i_dgp}.svma_FVR_cis, FVR_true(i_dgp,:)');
-    FVR_cov_svar(i_dgp,:) = cover2(dgps{i_dgp}.svar_FVR_cis, FVR_true(i_dgp,:)');
+    true_FVR(i_dgp,:) = dgps{i_dgp}.model.FVR(:,FVR_var);
+    cov_par_FVR(i_dgp,:) = cover2(dgps{i_dgp}.svma_FVR_cis, dgps{i_dgp}.model.FVR, dgps{i_dgp}.model.FVR);
+    cov_set_FVR(i_dgp,:) = cover2(dgps{i_dgp}.svma_FVR_cis, dgps{i_dgp}.bounds_pop.FVR_LB, dgps{i_dgp}.bounds_pop.FVR_UB);
+    cov_par_FVR_svar(i_dgp,:) = cover2(dgps{i_dgp}.svar_FVR_cis, dgps{i_dgp}.model.FVR, dgps{i_dgp}.model.FVR);
     
 end 
 
 % Create table
 results = table;
 results.dgp = dgp_no;
-results.R2_inv_true = R2_inv_true;
-results.R2_inv_cov = R2_inv_cov;
+results.true_R2_inv = true_R2_inv;
+results.cov_par_R2_inv = cov_par_R2_inv;
+results.cov_set_R2_inv = cov_set_R2_inv;
 for i_h=1:n_hor
-    results.(sprintf('%s%d%s', 'FVR', i_h, '_true')) = FVR_true(:,i_h);
-    results.(sprintf('%s%d%s', 'FVR', i_h, '_cov')) = FVR_cov(:,i_h);
-    results.(sprintf('%s%d%s', 'FVR', i_h, '_cov_svar')) = FVR_cov_svar(:,i_h);
+    results.(sprintf('%s%d', 'true_FVR', i_h)) = true_FVR(:,i_h);
+    results.(sprintf('%s%d', 'cov_par_FVR', i_h)) = cov_par_FVR(:,i_h);
+    results.(sprintf('%s%d', 'cov_set_FVR', i_h)) = cov_set_FVR(:,i_h);
+    results.(sprintf('%s%d%s', 'cov_par_FVR', i_h, '_svar')) = cov_par_FVR_svar(:,i_h);
 end
 
 % Print to screen
