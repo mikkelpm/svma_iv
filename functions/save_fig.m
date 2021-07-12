417d3efbcc098e52d5f6153f6961f9d0bc30b7a7
mikkelpm <mikkel.p.m@gmail.com>
2021-07-12 11:38:00 -0400

    Also save figures as .png and .fig

--- functions/save_fig.m
+++ functions/save_fig.m
@@ -2,8 +2,8 @@ function save_fig(folder,filename)
 
     % Save figure in various formats
 
-%     saveas(gcf, fullfile(folder, strcat(filename, '.fig')));
-%     saveas(gcf, fullfile(folder, strcat(filename, '.png')));
+    saveas(gcf, fullfile(folder, strcat(filename, '.fig')));
+    saveas(gcf, fullfile(folder, strcat(filename, '.png')));
     saveas(gcf, fullfile(folder, strcat(filename, '.eps')), 'epsc');
 
 end
